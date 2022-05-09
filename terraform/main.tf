232fe9fc34dbf7f1b641fac1e9cd881a210a3f15
Roberth Strand <me@robstr.dev>
2022-05-09 19:28:37 +0200

    added random

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.3"
+    }
+  }
+}
+
+resource "random_uuid" "test" {
+}
+
+output "test" {
+    value = random_uuid.test
+}
