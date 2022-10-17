8c0b94cd8db36a313e27c9f5c4b169b8eda3b7e1
Roberth Strand <me@robstr.dev>
2022-10-17 19:15:35 +0200

    fix(ex-dependency): moved subnet to vnet resource
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- examples/dependency/terraform/shared-resources/main.tf
+++ examples/dependency/terraform/shared-resources/main.tf
@@ -9,20 +9,14 @@ resource "azurerm_virtual_network" "base" {
   resource_group_name = azurerm_resource_group.base.name
   location            = azurerm_resource_group.base.location
   address_space       = ["10.0.0.0/16"]
-  tags                = local.tags
-}
 
-resource "azurerm_subnet" "base" {
-  name                 = format("snet-%s-%s", var.deployment_name, local.location)
-  resource_group_name  = azurerm_resource_group.base.name
-  virtual_network_name = azurerm_virtual_network.base.name
+  subnet {
+    name           = format("snet-%s-%s", var.deployment_name, local.location)
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.default.id
+  }
 
-  address_prefixes = ["10.0.0.0/24"]
-}
-
-resource "azurerm_subnet_network_security_group_association" "default" {
-  subnet_id                 = azurerm_subnet.base.id
-  network_security_group_id = azurerm_network_security_group.default.id
+  tags = local.tags
 }
 
 resource "azurerm_network_security_group" "default" {
