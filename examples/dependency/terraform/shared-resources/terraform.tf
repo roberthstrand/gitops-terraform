236a209227d5ac3d820d193beaf740ad02f95bd1
Roberth Strand <me@robstr.dev>
2022-10-17 18:59:39 +0200

    feat(ex-dependency): added tfc backend
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- examples/dependency/terraform/shared-resources/terraform.tf
+++ examples/dependency/terraform/shared-resources/terraform.tf
@@ -5,6 +5,12 @@ terraform {
       version = ">= 3.27.0"
     }
   }
+  cloud {
+    organization = "robstr"
+    workspaces {
+      name = "demo-gitops-terraform-shared-resources"
+    }
+  }
 }
 
 provider "azurerm" {
